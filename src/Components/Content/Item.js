89ee0a945e719860c11636f3ab95e2c09295a6f1
Hanna <hannapleshko2000@mail.ru>
2023-05-12 14:02:49 +0300

    pagination

--- src/Components/Content/Item.js
+++ src/Components/Content/Item.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import style from './style.module.css';
 
-function Item({ title }) {
+function Item({ vacancyItem }) {
   return (
-    <>
-      <p>{title}</p>
-    </>
+    <div className={style['wrapper']}>
+      <p>{vacancyItem.vacancy}</p>
+      <p>{vacancyItem.salary}</p>
+      <p>{vacancyItem.workday}</p>
+      <p>{vacancyItem.city}</p>
+    </div>
   );
 }
 
--- src/Components/Content/List.js
+++ src/Components/Content/List.js
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Pagination } from '@mantine/core';
 import Item from './Item';
-import srorage from '../../storage/storage.json';
-
-const filterCars = (searchText, listOfCars) => {
-  if (!searchText) {
-    return listOfCars;
-  }
-  return listOfCars.filter(({ car_model }) => car_model.toLowerCase().includes(searchText.toLowerCase()));
-};
+import storage from '../../storage/storage.json';
 
 function List({ searchString }) {
-  const [list, setList] = useState(srorage);
+  const [list, setList] = useState(storage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSizeRef = useRef(4);
+
+  const filterVacancy = () => {
+    if (!searchString) return storage;
+    return storage.filter(({ vacancy }) => vacancy.toLowerCase().includes(searchString.toLowerCase()));
+  };
+
+  const paginatedList = filterVacancy().slice((currentPage - 1) * pageSizeRef.current, currentPage * pageSizeRef.current);
 
   useEffect(() => {
-    console.log(searchString);
-
-    const Debounce = setTimeout(() => {
-      const filteredCars = filterCars(searchString, srorage);
-      setList(filteredCars);
-    }, 300);
-
-    return () => clearTimeout(Debounce);
+    setList(filterVacancy());
   }, [searchString]);
 
   return (
     <div>
-      {list.map((el, index) => (
-        <Item key={index} title={el.car_model} />
+      {paginatedList.map((el, index) => (
+        <Item key={index} vacancyItem={el} />
       ))}
+      <Pagination
+        total={Math.ceil(list.length / pageSizeRef.current)}
+        value={currentPage}
+        onChange={(page) => setCurrentPage(page)}
+        position="center"
+      />
     </div>
   );
 }
